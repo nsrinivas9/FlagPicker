c4faf241af981b2cfe178456ecb4eea42eff70ed
SRINIVASARAO NUTHALAPATI <SRINIVASARAON@hcl.com>
2019-08-09 16:50:36 +0530

    first commit

--- src/main/java/com/example/flagpicker/FlagPickerApplication.java
+++ src/main/java/com/example/flagpicker/FlagPickerApplication.java
@@ -1,8 +1,9 @@
 package com.example.flagpicker;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author SRINIVASARAON
@@ -10,14 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class FlagPickerApplication implements CommandLineRunner {
+public class FlagPickerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlagPickerApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	// TODO Auto-generated method stub
+	return builder.sources(FlagPickerApplication.class);
 }
+	}
